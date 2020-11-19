80cbb89d2f072a4c6aad13b0a1c711f439ee67a7
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2020-11-19 13:53:33 +0700

    Initialize My App, dan instalation fontAwseome, dan edit Readme

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState} from 'react';
+import React from 'react';
+import './App.css'
+import ListItems from './Listitems';
 
-// Komponen Popup
-function Alert() {
-  return <button onClick={() => alert('Thanks a lot...')}>Click Me.</button>
+
+class App extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+        items: [],
+        currentItems:{
+          text:'',
+          key:''
+        }
+      }
+
+     this.handleInput = this.handleInput.bind(this);
+     this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+        currentItems:{  
+          text: e.target.value,
+          key:Date.now()
+        }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItems;
+    // console.log(newItem);
+    if(newItem.text !== ''){
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItems: {
+          text: '',
+          key: ''
+        } 
+      })
+    }
+  }
+
+  render() { 
+    return ( 
+      <>
+       <header>
+          <h1>App Task</h1>
+          <form onSubmit={this.addItem }>
+            <input type='text' placeholder='Enter Task...' value={this.state.currentItems.text}
+            onChange={this.handleInput}
+            />
+            <button type='submit'>Add</button>
+          </form>
+      <ListItems items={this.state.items}/>
+      </header>
+      </>
+      
+     );
+  }
 }
-
-// Komponen Menyapa 
-function Greeting(props) {
-  return(
-    <div>
-      {props.kata}
-    </div>
-  );
-}
-
-// Komponen Menambah Angka
-const Contoh = () => {
-  const[count, setCount] = useState(0);
-  const[kurang, setKurang] = useState(0);
-
-  useEffect(() => {
-    document.title = `my-Project ${count}`
-  }) 
-
-  return <div>
-      <p>Tambah {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
-
-      <p>Kurang {kurang}</p>
-      <button onClick={() => setKurang(kurang - 1)}>-</button>
-  </div>
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Alert />
-      <Greeting kata='My-app' />
-      <Contoh />
-    </div>
-  );
-}
-
+ 
 export default App;
